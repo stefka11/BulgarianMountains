e0ba16b6c549cf251021393cd3f58cd6e52b98cd
stefka11 <sph_bg@abv.bg>
2025-03-31 18:51:08 +0300

    Add UserProvider and change App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router';
-
-import { UserContext } from './context/UserContext';
+import UserProvider from './providers/UserProvider';
 
 import Home from './components/home/Home';
 import Header from './components/header/Header';
@@ -12,31 +10,21 @@ import './App.css'
 import Catalog from './components/catalog/Catalog';
 
 function App() {
-  const [authData, setAuthData] = useState({});
-
-  const userLoginHandler = (resultData) => {
-    setAuthData(resultData);
-  };
-
-  const userLogoutHandler = () => {
-    setAuthData({});
-  };
-
-
   return (
-    < UserContext.Provider value={{ ...authData, userLoginHandler, userLogoutHandler }}>
+    <UserProvider>
       <div className="bg-white">
         <Header />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/catalog" element={<Catalog />} />
-        </Routes>
+        <main id="main-content">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/catalog" element={<Catalog />} />
+          </Routes>
+        </main>
       </div>
-      </UserContext.Provider>
+    </UserProvider>
   )
 }
 
---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import request from "../utils/request";
+
+export default function useAuth() {
+    const authData = useContext(UserContext);
+
+    const requestWrapper = (method, url, data, options = {}) => {
+        const authOptions = {
+            ...options,
+            headers: {
+                'X-Authorization': authData.accessToken,
+                ...options.headers
+            }
+        };
+
+        return request.baseRequest(method, url, data, authData.accessToken ? authOptions : options);
+    };
+
+    return {
+        ...authData,
+        userId: authData._id,
+        isAuthenticated: !!authData.accessToken,
+        request: {
+            get: requestWrapper.bind(null, 'GET'),
+            post: requestWrapper.bind(null, 'POST'),
+            put: requestWrapper.bind(null, 'PUT'),
+            delete: requestWrapper.bind(null, 'DELETE'),
+        }
+    }
+};
